c1506d654d1759d1fb9ad13b69d773fd4c6c9227
Francisco Miguel Villa <francisco.villa@novare.com.ph>
2025-02-10 11:07:31 +0800

    Updated commons.py with new code

---
+++ commons.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+import os
+
+MODEL_NAME = "gpt-4o-mini"
+EMBEDDING_NAME = "text-embedding-3-small"
+BASE_URL = "https://ainovate.novare.com.hk/"
+
+load_dotenv()
+def init_model() -> ChatOpenAI:
+    return ChatOpenAI(model=MODEL_NAME, base_url=BASE_URL)
+
+
+def init_embedding() -> OpenAIEmbeddings:
+    return OpenAIEmbeddings(model=EMBEDDING_NAME, base_url=BASE_URL)
+
+
+
