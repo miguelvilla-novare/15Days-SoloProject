7cf9b692e64c822514ed3b2806291c4046fd0629
Miguel Villa PC <villamico6@gmail.com>
2025-02-24 11:29:58 +0800

    Remove unnecessary files from repository

--- 1-code-resources/1-hello.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-
-load_dotenv()
-llm = ChatOpenAI(model="gpt-4o-mini",
-                 base_url="https://ainovate.novare.com.hk")
-
-print(llm.invoke("Hello world"))
